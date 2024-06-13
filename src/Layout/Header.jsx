f2511a32ae87d6a76e4ba5d7d8cd87cbe549f5b8
Reinald John Vibar <reinaldvibar@gmail.com>
2024-06-13 16:07:35 -0700

    test image display fixing

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,3 +1,5 @@
+import Logo from "./src/assets/logo/logo.webp";
+
 const Header = () => {
   return (
     <header>
@@ -6,12 +8,7 @@ const Header = () => {
           <nav className="font-inter mx-auto h-auto w-full max-w-[1600px] lg:relative lg:top-0">
             <div className="flex flex-col px-6 py-6 lg:flex-row lg:items-center lg:justify-between lg:px-10 lg:py-4 xl:px-20">
               <a href="#">
-                <img
-                  src={"src/assets/logo/logo.webp"}
-                  width={200}
-                  height={80}
-                  alt=""
-                />
+                <img src={Logo} width={200} height={80} alt="" />
               </a>
 
               <div className="mt-14 flex flex-col space-y-8 lg:mt-0 lg:flex lg:flex-row lg:space-x-1 lg:space-y-0">
